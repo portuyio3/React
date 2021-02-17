46a51e42235f3f002448d90b5587283843b1edf1
portuyio3 <76504777+portuyio3@users.noreply.github.com>
2021-02-17 12:19:46 +0700

    react

---
+++ src/Appheader.js
@@ -0,0 +1,13 @@
+function Appheader() {
+    
+
+
+return (
+    
+      <header className="App-header" src ="/pic/logo.png">
+
+       
+      </header>
+)       ;
+      }
+      export default Appheader;
---
+++ src/items.js
@@ -0,0 +1,27 @@
+const items =[
+    {
+      title :"man",
+      thumbnaiUrl :"/pic/son.jpg"
+      
+    
+    },
+    {
+      title :"gril",
+      thumbnaiUrl :"/pic/wome.jpeg"
+      
+    
+    },
+    {
+      title :"boy",
+      thumbnaiUrl :"/pic/guy.jpg"
+      
+    
+    },
+    {
+      title :"view",
+      thumbnaiUrl :"/pic/SSSS.jpg"
+      
+    
+    }
+  ];
+  export default items;
